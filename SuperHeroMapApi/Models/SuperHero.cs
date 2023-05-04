22310a6c78cd6f97b2ac57fdbdd4ea8b2b1e5243
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-05-05 00:50:09 +0200

    ajout model SuperHero

---
+++ SuperHeroMapApi/Models/SuperHero.cs
@@ -0,0 +1,13 @@
+﻿namespace SuperHeroMapApi.Models
+{
+    public class SuperHero
+    {
+        public int Id { get; set; } // ID unique du Super Héros dans la base de données
+        public string Name { get; set; } // Nom du Super Héros
+        public double Latitude { get; set; } // Latitude de la position géographique du Super Héros
+        public double Longitude { get; set; } // Longitude de la position géographique du Super Héros
+        public string PhoneNumber { get; set; } // Numéro de téléphone du Super Héros
+        public virtual ICollection<SuperHeroIncidentResource> SuperHeroIncidentResources { get; set; } // Collection de références aux SuperHeroIncidentResources associés à ce Super Héros
+        public virtual ICollection<SuperHeroIncident> SuperHeroIncidents { get; set; } // Collection de références aux SuperHeroIncidents associés à ce Super Héros
+    }
+}
