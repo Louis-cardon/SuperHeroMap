e1d8f2b0de57f907046877c08eb965651bca1249
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-05-05 00:49:32 +0200

    ajout model Incident

---
+++ SuperHeroMapApi/Models/Incident.cs
@@ -0,0 +1,14 @@
+﻿namespace SuperHeroMapApi.Models
+{
+    public class Incident
+    {
+        public int Id { get; set; } // ID unique de l'incident dans la base de données
+        public int ResourceId { get; set; } // ID de la ressource d'incident associée à cet incident
+        public string CityName { get; set; } // Nom de la ville associée à cet incident
+        public double Latitude { get; set; } // Latitude de la position géographique de cet incident
+        public double Longitude { get; set; } // Longitude de la position géographique de cet incident
+        public bool IsResolved { get; set; } // Booléen qui indique si l'incident a été résolu
+        public virtual IncidentResource IncidentResource { get; set; } // Référence à la ressource d'incident associée à cet incident
+        public virtual ICollection<SuperHeroIncident> SuperHeroIncidents { get; set; } // Collection de références aux SuperHeroIncidents associés à cet incident
+    }
+}
