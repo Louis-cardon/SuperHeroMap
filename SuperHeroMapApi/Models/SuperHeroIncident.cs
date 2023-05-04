1d2a15da02e27545e56e36bef62e7bd689c851d1
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-05-05 00:50:30 +0200

    ajout model SuperHeroIncident

---
+++ SuperHeroMapApi/Models/SuperHeroIncident.cs
@@ -0,0 +1,11 @@
+﻿namespace SuperHeroMapApi.Models
+{
+    public class SuperHeroIncident
+    {
+        public int Id { get; set; } // ID unique de la relation SuperHeroIncident dans la base de données
+        public int SuperHeroId { get; set; } // ID du Super Héros associé à cette relation
+        public int IncidentId { get; set; } // ID de l'incident associé à cette relation
+        public virtual SuperHero SuperHero { get; set; } // Référence au Super Héros associé à cette relation
+        public virtual Incident Incident { get; set; } // Référence à l'incident associé à cette relation
+    }
+}
