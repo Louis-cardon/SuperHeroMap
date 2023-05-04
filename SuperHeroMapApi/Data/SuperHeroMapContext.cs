aee573069283f6c9fcc90a63f21b7061e5258aed
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-05-05 00:53:10 +0200

    ajout dbContext

---
+++ SuperHeroMapApi/Data/SuperHeroMapContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SuperHeroMapApi.Data
+{
+    public class SuperHeroMapContext : DbContext
+    {
+
+        public SuperHeroMapContext(DbContextOptions<SuperHeroMapContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Incident> Incidents { get; set; }
+        public DbSet<IncidentResource> IncidentResources { get; set; }
+        public DbSet<SuperHero> SuperHeros { get; set; }
+        public DbSet<SuperHeroIncident> SuperHeroIncidents { get; set; }
+        public DbSet<SuperHeroIncidentResource> SuperHeroIncidentResources { get; set; }
+
+    }
+}
