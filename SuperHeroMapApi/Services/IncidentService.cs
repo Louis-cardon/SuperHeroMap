5e5b2c7579a0ef25937236d70d8c93aee39a2801
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-05-12 00:08:33 +0200

    ajout IncidentService

---
+++ SuperHeroMapApi/Services/IncidentService.cs
@@ -0,0 +1,49 @@
+﻿namespace SuperHeroMapApi.Services
+{
+    public interface IIncidentService
+    {
+        Task<IEnumerable<Incident>> GetIncidents();
+        Task<Incident> GetIncident(int id);
+        Task AddIncident(Incident incident);
+        Task UpdateIncident(Incident incident);
+        Task DeleteIncident(Incident incident);
+    }
+
+    public class IncidentService : IIncidentService
+    {
+        private readonly SuperHeroMapContext _context;
+
+        public IncidentService(SuperHeroMapContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Incident>> GetIncidents()
+        {
+            return await _context.Incidents.ToListAsync();
+        }
+
+        public async Task<Incident> GetIncident(int id)
+        {
+            return await _context.Incidents.FindAsync(id);
+        }
+
+        public async Task AddIncident(Incident incident)
+        {
+            _context.Incidents.Add(incident);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateIncident(Incident incident)
+        {
+            _context.Entry(incident).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteIncident(Incident incident)
+        {
+            _context.Incidents.Remove(incident);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
