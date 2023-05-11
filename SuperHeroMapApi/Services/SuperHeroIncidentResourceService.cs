7915d88697ded632b749d59365ad01e3c9903da1
Louis-cardon <93613237+Louis-cardon@users.noreply.github.com>
2023-05-12 01:16:13 +0200

    ajout SuperHeroIncidentResourceService

---
+++ SuperHeroMapApi/Services/SuperHeroIncidentResourceService.cs
@@ -0,0 +1,49 @@
+﻿namespace SuperHeroMapApi.Services
+{
+    public interface ISuperHeroIncidentResourceService
+    {
+        Task<IEnumerable<SuperHeroIncidentResource>> GetSuperHeroIncidentResources();
+        Task<SuperHeroIncidentResource> GetSuperHeroIncidentResource(int id);
+        Task AddSuperHeroIncidentResource(SuperHeroIncidentResource superHeroIncidentResource);
+        Task UpdateSuperHeroIncidentResource(SuperHeroIncidentResource superHeroIncidentResource);
+        Task DeleteSuperHeroIncidentResource(SuperHeroIncidentResource superHeroIncidentResource);
+    }
+
+    public class SuperHeroIncidentResourceService : ISuperHeroIncidentResourceService
+    {
+        private readonly SuperHeroMapContext _context;
+
+        public SuperHeroIncidentResourceService(SuperHeroMapContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<SuperHeroIncidentResource>> GetSuperHeroIncidentResources()
+        {
+            return await _context.SuperHeroIncidentResources.ToListAsync();
+        }
+
+        public async Task<SuperHeroIncidentResource> GetSuperHeroIncidentResource(int id)
+        {
+            return await _context.SuperHeroIncidentResources.FindAsync(id);
+        }
+
+        public async Task AddSuperHeroIncidentResource(SuperHeroIncidentResource superHeroIncidentResource)
+        {
+            _context.SuperHeroIncidentResources.Add(superHeroIncidentResource);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateSuperHeroIncidentResource(SuperHeroIncidentResource superHeroIncidentResource)
+        {
+            _context.Entry(superHeroIncidentResource).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteSuperHeroIncidentResource(SuperHeroIncidentResource superHeroIncidentResource)
+        {
+            _context.SuperHeroIncidentResources.Remove(superHeroIncidentResource);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
